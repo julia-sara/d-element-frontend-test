334531717630b69c0d7d243b9431550f4cb56423
julia-sara <sh.julljull@gmail.com>
2023-07-04 21:42:25 +0500

    feat: added text in html file

--- pages/homepage/src/index.js
+++ pages/homepage/src/index.js
@@ -1,2 +1,8 @@
-const text = 'Helloooo!';
-console.log(text);
+(function () {
+  const Button = document.querySelector(".burger-button");
+  const BurgContainer = document.querySelector(".burger-container");
+  Button.addEventListener("click", () => {
+    // B.classList.toggle('burger_activ');
+    BurgContainer.classList.toggle("burger-container_active");
+  });
+})();
